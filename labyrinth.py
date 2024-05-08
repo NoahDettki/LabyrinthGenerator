4d0d0ad3f40d21d08789ee29b883835f94f4f3f6
Noah Dettki <noah.dettki@gmail.com>
2024-05-08 15:13:35 +0200

    Refactoring

--- labyrinth.py
+++ labyrinth.py
@@ -42,7 +42,7 @@ class Tile:
             else:
                 return self.west
 
-
+# The following functions are not part of the Tile class
 def in_bounds(x: int, y: int) -> bool:
     '''Returns True if the given coordinates are within the labyrinth's bounds.
     Otherwise returns False'''
@@ -56,7 +56,6 @@ def process_tile(coordinates: tuple, queue: queue):
     '''The given coordinate is processed. Makes connections to neighbouring tiles with
     open connections and possibly creates open connections to empty neighbouring tiles
     in which case those empty tiles are put to the processing queue.'''
-    global color_value
     x, y = coordinates
     
     # A processed tile is neither empty nor queued anymore
@@ -70,82 +69,83 @@ def process_tile(coordinates: tuple, queue: queue):
         neigh_y = y + sequence[i][1]
 
         # Can only have connection to tile in bounds
-        if in_bounds(neigh_x, neigh_y):
-            # Can only have connection to unqueued tile (this prevents loops in the labyrinth)
-            if not lab[neigh_x][neigh_y].queued:
-                # Decide randomly if a connection to an empty neighbouring tile is made
-                if lab[neigh_x][neigh_y].empty:
-                    if random.random() <= config["CON_PROB"] or queue.qsize() <= config["LOOSE_ENDS"]:
-                        lab[x][y].open(sequence[i][0], sequence[i][1])
-                        queue.put((neigh_x, neigh_y))
-                        lab[neigh_x][neigh_y].queued = True
-                        connection_made = True
-                # Only make a connection to a processed neighbouring tile if that tile also has an open connection to this one
-                else:
-                    if lab[neigh_x][neigh_y].is_open(-sequence[i][0], -sequence[i][1]):
-                        lab[x][y].open(sequence[i][0], sequence[i][1])
-                        # Don't que neighbour tho because it was already processed
+        if not in_bounds(neigh_x, neigh_y):
+            continue
+        # Can only have connection to unqueued tile (this prevents loops in the labyrinth)
+        if lab[neigh_x][neigh_y].queued:
+            continue
 
-                        # Try to create a tunnel in the opposite direction of the connected neighbour
-                        # A tunnel can only be created if no random connection was made beforehand
-                        if connection_made:
-                            continue
-                        # A tunnel can only be made if there are no other tunnels in any orthogonal direction
-                        tunnel_found = False
-                        for j in range(config["WIDTH"]):
-                            if lab[j][y].tunnel:
-                                tunnel_found = True
-                                break
-                        for j in range(config["HEIGHT"]):
-                            if lab[x][j].tunnel:
-                                tunnel_found = True
-                                break
-                        if tunnel_found:
-                            continue
-                            
-                        # Loop through a line of tiles
-                        search_length = 0
-                        skipped_tiles = random.randint(config["MIN_TUN_DIS"], config["MAX_TUN_DIS"])
-                        search_location = (x + skipped_tiles * -sequence[i][0], y + skipped_tiles * -sequence[i][1])
+        # Decide randomly if a connection to an empty neighbouring tile is made
+        if lab[neigh_x][neigh_y].empty:
+            if random.random() <= config["CON_PROB"] or queue.qsize() <= config["LOOSE_ENDS"]:
+                lab[x][y].open(sequence[i][0], sequence[i][1])
+                queue.put((neigh_x, neigh_y))
+                lab[neigh_x][neigh_y].queued = True
+                connection_made = True
+        # Only make a connection to a processed neighbouring tile if that tile also has an open connection to this one
+        else:
+            if lab[neigh_x][neigh_y].is_open(-sequence[i][0], -sequence[i][1]):
+                lab[x][y].open(sequence[i][0], sequence[i][1])
+                # Don't que neighbour tho because it was already processed
+
+                # Try to create a tunnel in the opposite direction of the connected neighbour
+                # A tunnel can only be created if no random connection was made beforehand
+                if connection_made:
+                    continue
+                # A tunnel can only be made if there are no other tunnels in any orthogonal direction
+                tunnel_found = False
+                for j in range(config["WIDTH"]):
+                    if lab[j][y].tunnel:
+                        tunnel_found = True
+                        break
+                for j in range(config["HEIGHT"]):
+                    if lab[x][j].tunnel:
+                        tunnel_found = True
+                        break
+                if tunnel_found:
+                    continue
+                    
+                # Loop through a line of tiles
+                search_length = 0
+                skipped_tiles = random.randint(config["MIN_TUN_DIS"], config["MAX_TUN_DIS"])
+                search_location = (x + skipped_tiles * -sequence[i][0], y + skipped_tiles * -sequence[i][1])
+                to_be_tunnel = None
+                while True:
+                    # Pay attention to the maximum tunnel distance
+                    if search_length >= config["MAX_TUN_DIS"]:
+                        break
+                    # Move to next tile
+                    search_location = (search_location[0] + -sequence[i][0], search_location[1] + -sequence[i][1])
+                    search_length += 1
+                    # Stop searching if the bounds of the labyrinth are reached
+                    if not in_bounds(search_location[0], search_location[1]):
+                        break
+                    current_tile = lab[search_location[0]][search_location[1]]
+                    # Is the current tile empty?
+                    if current_tile.empty and not current_tile.queued:
+                        if to_be_tunnel == None:
+                            # This tile is empty but no position for the tunnel was yet determined
+                            to_be_tunnel = current_tile
+                        else:
+                            # This tile is empty and a tunnel position was already determined, so the current tile has to be queued
+                            queue.put((search_location[0], search_location[1]))
+                            current_tile.queued = True
+                            # Additionally the tunnel has to be created
+                            to_be_tunnel.empty = False
+                            to_be_tunnel.tunnel = True
+                            to_be_tunnel.open(-sequence[i][0], -sequence[i][1])
+                            lab[x][y].tunnel = True
+                            break
+                    else:
                         to_be_tunnel = None
-                        while True:
-                            # Pay attention to the maximum tunnel distance
-                            if search_length >= config["MAX_TUN_DIS"]:
-                                break
-                            # Move to next tile
-                            search_location = (search_location[0] + -sequence[i][0], search_location[1] + -sequence[i][1])
-                            search_length += 1
-                            # Stop searching if the bounds of the labyrinth are reached
-                            if not in_bounds(search_location[0], search_location[1]):
-                                break
-                            current_tile = lab[search_location[0]][search_location[1]]
-                            # Is the current tile empty?
-                            if current_tile.empty and not current_tile.queued:
-                                if to_be_tunnel == None:
-                                    # This tile is empty but no position for the tunnel was yet determined
-                                    to_be_tunnel = current_tile
-                                else:
-                                    # This tile is empty and a tunnel position was already determined, so the current tile has to be queued
-                                    queue.put((search_location[0], search_location[1]))
-                                    current_tile.queued = True
-                                    # Additionally the tunnel has to be created
-                                    to_be_tunnel.empty = False
-                                    to_be_tunnel.tunnel = True
-                                    to_be_tunnel.open(-sequence[i][0], -sequence[i][1])
-                                    lab[x][y].tunnel = True
-                                    break
-                            else:
-                                to_be_tunnel = None
-
-                        # After creating a tunnel, no other connections can be made (so break the loop)
-                        if lab[x][y].tunnel: break
-
+                # After creating a tunnel, no other connections can be made (so break the loop)
+                if lab[x][y].tunnel: break
     # Shuffle direction sequence
     random.shuffle(sequence)
 
 
 ############################## PROGRAM START #######################################################            
-# load json config
+# Load json config
 with open('config.json', 'r') as f:
     config = json.load(f)
 
@@ -188,27 +188,28 @@ while empty_exists:
         for y in range(config["HEIGHT"]):
             random.shuffle(sequence)
             current_tile = lab[x][y]
-            # check if this tile is empty
-            if current_tile.empty:
-                # search a neighbouring tile that is not empty
-                found = False
-                for i in range(len(sequence)):
-                    neigh_x = x + sequence[i][0]
-                    neigh_y = y + sequence[i][1]
-                    if not in_bounds(neigh_x, neigh_y):
-                        continue
-                    neigbour = lab[neigh_x][neigh_y]
-                    if not neigbour.empty and not neigbour.tunnel:
-                        current_tile.open(sequence[i][0], sequence[i][1])
-                        current_tile.empty = False
-                        neigbour.open(-sequence[i][0], -sequence[i][1])
-                        found = True
-                        break
-                if not found:
-                    empty_exists = True
+            # Only process empty tiles
+            if not current_tile.empty:
+                continue
+            # Search a neighbouring tile that is not empty
+            found = False
+            for i in range(len(sequence)):
+                neigh_x = x + sequence[i][0]
+                neigh_y = y + sequence[i][1]
+                if not in_bounds(neigh_x, neigh_y):
+                    continue
+                neigbour = lab[neigh_x][neigh_y]
+                if not neigbour.empty and not neigbour.tunnel:
+                    current_tile.open(sequence[i][0], sequence[i][1])
+                    current_tile.empty = False
+                    neigbour.open(-sequence[i][0], -sequence[i][1])
+                    found = True
+                    break
+            if not found:
+                empty_exists = True
 
 # Step 3: Generate image out of the 2d list
-img = Image.new(mode = "RGBA",
+img = Image.new(mode = "RGB",
                 size = [config['WIDTH'] * TILE_SIZE, config['HEIGHT'] * TILE_SIZE], 
                 color = (255, 255, 255))
 for x in range(config["WIDTH"]):
